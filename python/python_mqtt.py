699611ed47b9c3b4ec17cc3e3c3c468ddb63a709
Sander Arjan Benoist <sabenoist5@gmail.com>
2021-01-04 14:17:24 +0100

    fixed MQTT broker port on client side

--- python/python_mqtt.py
+++ python/python_mqtt.py
@@ -1,34 +1,47 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho
 import time
 
-# everything works so far, but the function is not being triggered 
+broker = "127.0.0.1"
+port = 1883
+client = paho.Client("local")
+topic = "sensor_001"
 
-#####
-def on_message(client, userdata, message):
-    print("message received, ", str(message.payload.decode("utf-8")))
-    print("message topic = ", message.topic)
-    print("message qos = ", message.qos)
-#####
+def on_log(client, userdata, level, buff):  # mqtt logs function
+    print(buff)
 
-# Choosing address, same as publisher
-#broker_address = "mongodb://127.0.0.1:27017/MQTT" #this might need to change
-broker_address = "127.0.0.1"
-# Create a new instance
-print("creating new instance")
-client = mqtt.Client("P1")
+def on_connect(client, userdata, flags, rc):  # connect to mqtt broker function
+    if rc == 0:
+        client.connected_flag = True  # set flags
+        print("Connected Info")
+    else:
+        print("Bad connection returned code = " + str(rc))
+        client.loop_stop()
 
-# Attatching function
-print("attatching function")
-client.on_message = on_message
-# Connect to the broker
-print("connect to the broker")
-client.connect(broker_address, port=1833)
-client.loop_start()
+def on_disconnect(client, userdata, rc):  # disconnect to mqtt broker function
+    print("Client disconnected OK")
 
-# Subscribe to the topic
-print("subscribing to topic")
-client.subscribe("sensor_001")
+def on_publish(client, userdata, mid):  # publish to mqtt broker
+    print("In on_pub callback mid=" + str(mid))
 
-time.sleep(20)#wait
+def on_subscribe(client, userdata, mid, granted_qos):  # subscribe to mqtt broker
+    print("Subscribed", userdata)
 
-client.loop_stop() #stop the loop
+def on_message(client, userdata, message):  # get message from mqtt broker 
+    print("New message received: ", str(message.payload.decode("utf-8")), "Topic : %s ", message.topic, "Retained : %s", message.retain)
+
+def connectToMqtt():  # connect to MQTT broker main function
+    print("Connecting to MQTT broker")
+    # client.username_pw_set(username="", password="")
+    client.on_log = on_log
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    client.on_subscribe = on_subscribe
+
+
+    client.connect(broker, port, keepalive=600)
+    ret = client.subscribe(topic, qos=0)
+    print("Subscribed return = " + str(ret))
+    client.on_message = on_message
+
+connectToMqtt()  # connect to mqtt broker
+client.loop_forever()
